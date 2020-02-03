619386c33173c821926930b2b1155f40f93f0a8c
Manish <mchaurasia@gotprint.net>
2020-02-03 11:49:49 +0530

    Inital Commit

---
+++ app/src/main/java/com/vertechxa/fragmentintro/MainActivity.java
@@ -0,0 +1,25 @@
+package com.vertechxa.fragmentintro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.myContainer);
+        if(fragment == null) {
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.myContainer, fragment)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/vertechxa/fragmentintro/MainFragment.java
@@ -0,0 +1,28 @@
+package com.vertechxa.fragmentintro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * @author robert
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return  view;
+    }
+}
